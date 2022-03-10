51bceaf81f0293008f2d6efa380b67a4fae074a6
ImDuong <shiroyasha699969@gmail.com>
2022-03-10 17:40:08 +0700

    remove redundant code, add a view message

--- src/App.java
+++ src/App.java
@@ -1,22 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Dz khong yeu Linh mot chut nao ca Dz gia doi");
-        System.out.println("Dz yeu linh");
-	System.out.println("Em khong yeu anhhh");
-        System.out.println("Hello, World!");
-        System.out.println("Dz có yêuuuuu Linh");  
-        int firstNumber = add(6, 9);
-        int secondNumber = mul(6, 9);
-        int finalNumber = add(firstNumber, secondNumber);
-	    System.out.println(finalNumber);
-    }
-
-    public static int add(int a, int b) {
-        return a + b;
-    }
-
-    public static int mul(int a, int b) {
-        return a * b;
+        System.out.println("Starting server");
     }
 
 }
