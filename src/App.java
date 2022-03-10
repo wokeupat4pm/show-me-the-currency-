9b0a1e3b8e4ecab194f82111b2da61eb551e008f
ImDuong <shiroyasha699969@gmail.com>
2022-03-10 16:29:56 +0700

    add multiplication and addition into app.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        int firstNumber = add(6, 9);
+        int secondNumber = mul(6, 9);
+        int finalNumber = add(firstNumber, secondNumber);
+	    System.out.println(finalNumber);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int mul(int a, int b) {
+        return a * b;
     }
 }
