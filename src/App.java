313a6c8f90746096866e93668e3d6f042875cf5b
Thai Duong <33058526+ImDuong@users.noreply.github.com>
2022-03-10 17:11:07 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Dz yeu linh");
+	System.out.println("Em khong yeu anhhh");
         System.out.println("Hello, World!");
         System.out.println("Dz có yêuuuuu Linh");  
         int firstNumber = add(6, 9);
